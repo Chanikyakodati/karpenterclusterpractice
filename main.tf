7ee645ae0d245046bc448b710a4b17b7071f4eaa
Chanikyakodati <chanikya.kodati@gmail.com>
2024-10-01 19:11:38 -0400

    removed the unnecessary changes in karperter

--- main.tf
+++ main.tf
@@ -246,31 +246,6 @@ resource "kubectl_manifest" "karpenter_node_class" {
   ]
 }
 
-/* Second NodeClass */
-resource "kubectl_manifest" "karpenter_node_class2" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.k8s.aws/v1beta1
-    kind: EC2NodeClass
-    metadata:
-      name: default
-    spec:
-      amiFamily: AL2023
-      role: ${module.karpenter.node_iam_role_name}
-      subnetSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: ${module.eks.cluster_name} + "2"
-      securityGroupSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: ${module.eks.cluster_name} + "2"
-      tags:
-        karpenter.sh/discovery: ${module.eks.cluster_name} + "2"
-  YAML
-
-  depends_on = [
-    helm_release.karpenter
-  ]
-}
-
 
 resource "kubectl_manifest" "karpenter_node_pool" {
   yaml_body = <<-YAML
@@ -307,42 +282,4 @@ resource "kubectl_manifest" "karpenter_node_pool" {
     kubectl_manifest.karpenter_node_class
   ]
 }
-
-/* Second Node Pool */
-resource "kubectl_manifest" "karpenter_node_pool2" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1beta1
-    kind: NodePool
-    metadata:
-      name: default
-    spec:
-      template:
-        spec:
-          nodeClassRef:
-            name: default
-          requirements:
-            - key: "karpenter.k8s.aws/instance-category"
-              operator: In
-              values: ["c", "m", "r"]
-            - key: "karpenter.k8s.aws/instance-cpu"
-              operator: In
-              values: ["4", "8", "16", "32"]
-            - key: "karpenter.k8s.aws/instance-hypervisor"
-              operator: In
-              values: ["nitro"]
-            - key: "karpenter.k8s.aws/instance-generation"
-              operator: Gt
-              values: ["2"]
-      limits:
-        cpu: 1000
-      disruption:
-        consolidationPolicy: WhenEmpty
-        consolidateAfter: 30s
-  YAML
-
-  depends_on = [
-    kubectl_manifest.karpenter_node_class2
-  ]
-}
-
 
