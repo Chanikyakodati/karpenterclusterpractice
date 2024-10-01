7d56eb9715064a1b926d340dac70dfb18e3cb25b
Chanikyakodati <chanikya.kodati@gmail.com>
2024-10-01 12:41:53 -0400

    placed environment variables

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   alias  = "virginia"
+  access_key = "${AWS_ACCESS_KEY_ID}"
+  secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 
 data "aws_availability_zones" "available" {}
